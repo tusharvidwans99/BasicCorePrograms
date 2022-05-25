d307f2751161c4afa97aa13c5a29faed20837d43
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-05-25 11:50:47 +0530

    [Tushar Vidwans] Welcome message in the Basic Core Programs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace BasicCorePrograms
+{
+    public class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Basic Core Prograams");
+            
+        }
+    }
+}
